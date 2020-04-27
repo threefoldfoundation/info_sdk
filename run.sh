c2778309d66d3f736b2df2526175b76714b89e2b
kristof <kristof@incubaid.com>
2020-04-27 10:43:05 +0200

    ...

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-cd ~/code/github/threefoldfoundation/info_tfgridsdk/src
-open http://localhost:3000/index.html#/intro
-tfwiki -s -d .
+cd ~/code/github/threefoldfoundation/info_tfgridsdk
+open http://localhost:3000/sdk/index.html#/intro
+tfweb -c threefold_sites.toml
 
